0d99bee8bf65a2ebfef10aaad1780f193966d0b5
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-13 01:18:29 -0500

    current weather box
    ⛈☂️🔥 css html

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -5,6 +5,13 @@
 $( document ).ready(() => {
     console.log("Yay pocket weather!")
 
+
+    let today = processDay()
+
+
+    $("#todaysWeather").text(`${today} - `)
+
+
  
 
     $("#headDiv").click( (e) => {
@@ -13,7 +20,7 @@ $( document ).ready(() => {
 
         switch(e.target.id) {
             case "searchCityEmoji":
-                searchCity($( "#searchCityInput").val())
+                searchCity($( "#searchCityInput").val(), today)
                 break;
             case "newEvent":
                 // run function!
@@ -25,14 +32,51 @@ $( document ).ready(() => {
     })
 
 
+})
 
-
-    const searchCity = (city) => {
+    const searchCity = (city, today) => {
         console.log(city)
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=9d900c46a82ac29300d02baa0107cbe8')
+        // console.log(today)
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=atlanta&units=imperial&appid=9d900c46a82ac29300d02baa0107cbe8`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            $("#currentCity").text(`${today} in ${data.name} -`)
+            $("#todaysWeather").text(data.weather[0].description)
+            $("#temperature").text(data.main.temp)
+
+
         })
     }
-})
+
+
+
+    const processDay = () => {
+        let date = new Date ()
+        let todaysNumber = date.getDay()
+        
+        switch(todaysNumber) {
+            case 1:
+                return "Monday"
+            break;
+            case 2:
+                return "Tuesday"
+            break;
+            case 3:
+                return "Wednesday"
+            break;
+            case 4:
+                return "Thursday"
+            break;
+            case 5:
+                return "Friday"
+            break;
+            case 6:
+                return "Saturday"
+            case 7:
+                return "Sunday"
+            break; 
+        }
+    }
+
